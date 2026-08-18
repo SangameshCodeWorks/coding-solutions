866491536963629619ecedf70f2b5986054dd8e9
Sangamesh Jainapur <sanguforwork123@gmail.com>
2026-08-18 23:10:20 +0530

    ✅ Sales by Match | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-18

---
+++ hackerrank/medium/sock-merchant/solution.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+from collections import Counter
+
+def sockMerchant(n, ar):
+    sock_counts = Counter(ar)
+    total_pairs = sum(count // 2 for count in sock_counts.values())
+    return total_pairs
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
